39cc52c98d3c54dddf790443000ebba54567788f
sergiolpzgmz <sergiolopezgmz@gmail.com>
2023-07-19 12:37:17 +0200

    View code optimization

--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -9,89 +9,67 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 public class View extends JFrame {
-
+    public JTextField txtName;
+    public JTextArea descriptiontxt;
+    public JDateChooser dateChooser;
+    public Choice priorityChoice;
     public View(){
+
+        JPanel contentPane = createMainPannel();
+
+        createNewTaskView(contentPane);
+
+        createAddOrCancelView(contentPane);
+
+        createTableTaskView(contentPane);
+    }
+    private JPanel createMainPannel() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100,100,450,521);
 
         JPanel contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
         setContentPane(contentPane);
         contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.PAGE_AXIS));
+        return contentPane;
+    }
 
-
-        // Create task view
+    private void createNewTaskView(JPanel contentPane) {
         JPanel panelProject = new JPanel();
         panelProject.setBorder(new TitledBorder(null, "Add Task", TitledBorder.LEADING, TitledBorder.TOP, null, null));
         contentPane.add(panelProject);
         panelProject.setLayout(new GridLayout(8, 8));
 
+        createLabel("Name:", panelProject);
 
-        JLabel lblNewLabel = new JLabel("Name:");
-        panelProject.add(lblNewLabel);
-
-        JTextField txtName = new JTextField();
+        txtName = new JTextField();
         txtName.setHorizontalAlignment(SwingConstants.LEFT);
         panelProject.add(txtName);
         txtName.setColumns(10);
 
-        Component verticalStrut = Box.createVerticalStrut(5);
-        panelProject.add(verticalStrut);
-        verticalStrut = Box.createVerticalStrut(5);
-        panelProject.add(verticalStrut);
+        spaceBetweenElements(panelProject);
 
-        JLabel lblNewLabel_1 = new JLabel("Description:");
-        panelProject.add(lblNewLabel_1);
+        createLabel("Description:", panelProject);
 
-        JTextArea txtDescription = new JTextArea();
-        panelProject.add(txtDescription);
+        descriptiontxt = new JTextArea();
+        panelProject.add(descriptiontxt);
 
-        verticalStrut = Box.createVerticalStrut(5);
-        panelProject.add(verticalStrut);
-        verticalStrut = Box.createVerticalStrut(5);
-        panelProject.add(verticalStrut);
+        spaceBetweenElements(panelProject);
 
+        createLabel("Deadline:", panelProject);
 
-        JLabel lblNewLabel_2 = new JLabel("Deadline:");
-        panelProject.add(lblNewLabel_2);
-
-        JDateChooser dateChooser = new JDateChooser();
+        dateChooser = new JDateChooser();
         panelProject.add(dateChooser);
 
-        verticalStrut = Box.createVerticalStrut(5);
-        panelProject.add(verticalStrut);
-        verticalStrut = Box.createVerticalStrut(5);
-        panelProject.add(verticalStrut);
+        spaceBetweenElements(panelProject);
 
-        JLabel lblNewLabel_4 = new JLabel("Priority:");
-        panelProject.add(lblNewLabel_4);
+        createLabel("Priority:", panelProject);
 
-        Box horizontalBox = Box.createHorizontalBox();
-        panelProject.add(horizontalBox);
+        priorityChoice = new Choice();
+        panelProject.add(priorityChoice);
+    }
 
-        JRadioButton rdbtnHigh = new JRadioButton("High");
-        rdbtnHigh.setAlignmentX(Component.CENTER_ALIGNMENT);
-        rdbtnHigh.setHorizontalAlignment(SwingConstants.CENTER);
-        horizontalBox.add(rdbtnHigh);
-
-        JRadioButton rdbtnMid = new JRadioButton("Mid");
-        rdbtnMid.setAlignmentX(Component.CENTER_ALIGNMENT);
-        rdbtnMid.setHorizontalAlignment(SwingConstants.CENTER);
-        horizontalBox.add(rdbtnMid);
-
-        JRadioButton rdbtnLow = new JRadioButton("Low");
-        rdbtnMid.setAlignmentX(Component.CENTER_ALIGNMENT);
-        rdbtnMid.setHorizontalAlignment(SwingConstants.CENTER);
-        horizontalBox.add(rdbtnLow);
-
-        ButtonGroup buttonGroup = new ButtonGroup();
-        buttonGroup.add(rdbtnHigh);
-        buttonGroup.add(rdbtnMid);
-        buttonGroup.add(rdbtnLow);
-
-
-        // Add options
+    private static void createAddOrCancelView(JPanel contentPane) {
         JPanel panel = new JPanel();
         contentPane.add(panel);
         panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
@@ -104,7 +82,9 @@ public class View extends JFrame {
 
         JButton btnCancel = new JButton("Cancel");
         panel.add(btnCancel);
-        // View all tasks
+    }
+
+    private static void createTableTaskView(JPanel contentPane) {
         JPanel panelData = new JPanel();
         contentPane.add(panelData);
         panelData.setBorder(new TitledBorder(null, "Tasks", TitledBorder.LEADING, TitledBorder.TOP, null, null));
@@ -118,9 +98,23 @@ public class View extends JFrame {
                 new Object[][] {
                 },
                 new String[] {
-                        "NAME", "DESCRIPTION", "PROJECT MANAGER", "DEADLINE", "FINISHED"
+                        "NAME", "DESCRIPTION","DEADLINE", "Priority"
                 }
         ));
         scrollPane.setViewportView(projectsTable);
+    }
+
+    /*This method create vertical margin between gui elements
+     *  -receives the panel on which the margin is to be created */
+    private static void spaceBetweenElements(JPanel panelProject) {
+        Component verticalStrut = Box.createVerticalStrut(5);
+        panelProject.add(verticalStrut);
+        verticalStrut = Box.createVerticalStrut(5);
+        panelProject.add(verticalStrut);
+    }
+
+    private static void createLabel(String text, JPanel panelProject) {
+        JLabel namelbl = new JLabel(text);
+        panelProject.add(namelbl);
     }
 }
