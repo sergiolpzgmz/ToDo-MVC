db1eeb9654b4a657fc9ab9b85829d685e7abec7d
sergiolpzgmz <sergiolopezgmz@gmail.com>
2023-09-28 21:21:57 +0200

    Improved UML, added testing to connection class

---
+++ src/test/java/Util/DataBaseConnectionTest.java
@@ -0,0 +1,24 @@
+package Util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataBaseConnectionTest {
+
+    @Test
+    void testGetInstance() {
+        try {
+            Connection connection = DataBaseConnection.getInstance();
+            assertNotNull(connection);
+
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
